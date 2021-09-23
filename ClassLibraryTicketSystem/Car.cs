5a525cdcc31d4e098955e0e591b74e7d1557fb64
tyjuji <tyjuji@gmx.com>
2021-09-23 16:14:48 +0200

    Task 1 and 2 completed.

---
+++ ClassLibraryTicketSystem/Car.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryTicketSystem
+{
+    /// <summary>
+    /// Describes an automobile.
+    /// </summary>
+    public class Car
+    {
+        /// <summary>
+        /// A 7 character string identifying the vehicle.
+        /// </summary>
+        public string Licenseplate { get; set; }
+
+        /// <summary>
+        /// Date and time of passing over the bridge.
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Price for passing over the bridge in this vehicle.
+        /// </summary>
+        /// <returns>240. Price is fixed.</returns>
+        public double Price()
+        {
+            return 240;
+        }
+
+        /// <summary>
+        /// Returns the type that defines the vehicle.
+        /// </summary>
+        /// <returns>"Car"</returns>
+        public string VehicleType()
+        {
+            return "Car";
+        }
+    }
+}
---
+++ ClassLibraryTicketSystemTests/CarTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibraryTicketSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryTicketSystem.Tests
+{
+    [TestClass()]
+    public class CarTests
+    {
+        [TestMethod()]
+        public void PriceTest()
+        {
+            Car car = new Car();
+
+            var result = car.Price();
+
+            Assert.AreEqual(240, result);
+        }
+
+        [TestMethod()]
+        public void VehicleTypeTest()
+        {
+            Car car = new Car();
+
+            var result = car.VehicleType();
+
+            Assert.AreEqual("Car", result);
+        }
+    }
+}
